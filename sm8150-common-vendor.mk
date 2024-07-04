55f080ab947c86d9ef1a2101be2dbcbccc9948ec
Han Sol Jin <hansol@hansol.ca>
2024-07-04 12:27:29 -0700

    sm8150-common: add EGL symlinks
    Change-Id: I68e320898d216db9a7477cf9cb1af3cea4a3ace7

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -1140,3 +1140,11 @@ PRODUCT_PACKAGES += \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service \
     vendor.qti.hardware.display.allocator-service
+
+PRODUCT_PACKAGES += \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
